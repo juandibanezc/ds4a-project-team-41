ab7fbceef65cb44fdf4eb9730a7f5c2bebccd02d
Alfonso <aserrano@itwbrands.com>
2022-06-11 17:45:05 -0500

    Alfonso Update1

---
+++ app.py
@@ -0,0 +1,14 @@
+from dash import Dash, html, dcc
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+                html.Div(
+                    id = "sidebar-menu",
+                    className="light",
+                    children=[
+                        html.Figure(
+                            
+                        )
+                    ])
+            ])
